5e1134722cb118fbb399e2ee85d39d7d33c43d40
shiwan <shiwanfoo@hotmail.com>
2026-02-13 15:10:20 +0800

    Added functionality to change date range for viewing timer completion history

--- README.md
+++ README.md
@@ -6,11 +6,7 @@
 - [X] Include 4 different timer profiles (up to 60 minutes)
 - [X] Short chime when timer ends 
 - [X] Settings to change timer durations and chime sound 
-- [X] Viewing of timer completion recent stats 
-
-## Pending Features
-- [ ] Allow flexible dates when viewing timer completion stats 
-- [ ] Styling of widgets 
+- [X] Viewing of timer completion recent stats with flexible date range
 
 ## Dependencies:
 - `pyside6`
--- src/data.py
+++ src/data.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QDialog, QComboBox, QVBoxLayout, QLabel, QDateEdit, QWidget, QHBoxLayout, QPushButton, QSizePolicy
-from PySide6.QtCore import QDate
+from PySide6.QtWidgets import QDialog, QComboBox, QVBoxLayout, QLabel, QDateEdit, QWidget, QHBoxLayout, QPushButton
+from PySide6.QtCore import QDate, Slot, Signal, Qt
 import pandas as pd 
 import matplotlib
 import matplotlib.pyplot as plt 
@@ -12,8 +12,9 @@ date_format = "%d/%m/%Y"
 matplotlib.use("QtAgg")
 plt.style.use("seaborn-v0_8-darkgrid")
 
+
 class MplCanvas(FigureCanvasQTAgg):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
+    def __init__(self, parent=None, width=8, height=4, dpi=100):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes=self.fig.add_subplot(111)
         super().__init__(self.fig)
@@ -24,41 +25,106 @@ class CustomDateEditor(QWidget):
         super().__init__()
         self.my_layout = QHBoxLayout(self)
         q_date_today = QDate(today_date.year, today_date.month, today_date.day)
+
         self.start_date_editor = QDateEdit(q_date_today)
+        self.start_date_editor.setMaximumDate(q_date_today)
         self.end_date_editor = QDateEdit(q_date_today)
+        self.end_date_editor.setMaximumDate(q_date_today)
+
         self.refresh_button = QPushButton("Refresh")
-        self.my_layout.addWidget(QLabel("Start:"))
-        self.my_layout.addWidget(self.start_date_editor)
-        self.my_layout.addWidget(QLabel("End:"))
-        self.my_layout.addWidget(self.end_date_editor)
+
+        self.my_layout.addWidget(QLabel("Start:"), alignment=Qt.AlignmentFlag.AlignRight)
+        self.my_layout.addWidget(self.start_date_editor, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.my_layout.addWidget(QLabel("End:"), alignment=Qt.AlignmentFlag.AlignRight)
+        self.my_layout.addWidget(self.end_date_editor, alignment=Qt.AlignmentFlag.AlignLeft)
         self.my_layout.addWidget(self.refresh_button)
 
+
 class DataDialog(QDialog):
+    new_date_range = Signal(datetime, datetime)
+
     def __init__(self, df, parent=None):
         super().__init__(parent=parent)
         self.df = df 
+        self.new_date_range.connect(self.update_plot)
 
         datetime_today = datetime.now()
-        datetime_date = datetime(datetime_today.year, datetime_today.month, datetime_today.day)
-        filtered_df, total_mins, avg_mins = self.filter_df_by_date(datetime_date + timedelta(-6), datetime_date) # Show last 7 days by default 
+        self.datetime_today_date = datetime(datetime_today.year, datetime_today.month, datetime_today.day)
         
         self.choices_dropdown = QComboBox()
         self.choices_dropdown.insertItems(0, ["Last 7 days", "Last 30 days", "Custom"])
-        self.choices_dropdown.setEnabled(False) # Remove this once functionality is implemented 
+        self.choices_dropdown.currentTextChanged.connect(self.dropdown_selection_changed)
 
         self.custom_date_editor = CustomDateEditor(datetime_today)
         self.custom_date_editor.hide()
+        self.custom_date_editor.refresh_button.released.connect(self.custom_date_range)
 
-        sc = MplCanvas(self)
-        filtered_df.plot(ax=sc.axes, marker="o")
-        sc.axes.set_xticklabels(sc.axes.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
-        sc.axes.set_xlabel(None)
-        sc.axes.set_ylabel("Minutes")
-        sc.axes.get_legend().remove()
-        for x, y in enumerate(filtered_df["Minutes"]):
-            sc.axes.annotate(f"{y}", (x, y), textcoords='data', fontsize=12)
-        sc.fig.tight_layout()
+        self.total_label = QLabel("")
+        self.avg_label = QLabel("")
+
+        self.sc = MplCanvas(self)
+        self.update_plot(self.datetime_today_date + timedelta(-6), self.datetime_today_date) # Show last 7 days by default 
+
+        self.dialog_layout = QVBoxLayout(self)
+        self.dialog_layout.addWidget(self.choices_dropdown)
+        self.dialog_layout.addWidget(self.custom_date_editor)
+        self.dialog_layout.addWidget(self.sc)
+        self.dialog_layout.addWidget(self.total_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.dialog_layout.addWidget(self.avg_label, alignment=Qt.AlignmentFlag.AlignCenter)
         
+
+    def filter_df_by_date(self, startdate: datetime, enddate: datetime):
+        filtered_df = self.df[(self.df.index >= startdate) & (self.df.index <= enddate)]
+
+        while startdate <= enddate:
+            if not startdate in filtered_df.index:
+                filtered_df.loc[startdate] = 0 
+            startdate += timedelta(1)
+
+        filtered_df = filtered_df.sort_index()
+        filtered_df.index = filtered_df.index.strftime(f"{date_format} (%a)")
+
+        return filtered_df, filtered_df["Minutes"].sum(), round(filtered_df["Minutes"].mean()) 
+    
+
+    @Slot()
+    def dropdown_selection_changed(self, new_text: str):
+        if new_text == "Custom":
+            self.custom_date_editor.show()
+
+        elif new_text == "Last 30 days":
+            self.new_date_range.emit(self.datetime_today_date - timedelta(29), self.datetime_today_date)
+            self.custom_date_editor.hide() 
+        
+        elif new_text == "Last 7 days":
+            self.new_date_range.emit(self.datetime_today_date - timedelta(6), self.datetime_today_date)
+            self.custom_date_editor.hide()
+
+    
+    @Slot()
+    def custom_date_range(self):
+        q_start_date = self.custom_date_editor.start_date_editor.date()
+        q_end_date = self.custom_date_editor.end_date_editor.date()
+        self.new_date_range.emit(datetime(*q_start_date.getDate()), datetime(*q_end_date.getDate()))
+
+
+    @Slot()
+    def update_plot(self, start_date: datetime, end_date: datetime):
+        filtered_df, total_mins, avg_mins = self.filter_df_by_date(start_date, end_date)
+        x_size = filtered_df.index.size
+        
+        self.sc.axes.cla()
+        filtered_df.plot(ax=self.sc.axes, marker="o")
+        self.sc.axes.set_xticks(range(x_size))
+        self.sc.axes.set_xticklabels(filtered_df.index, rotation=45, ha='right', rotation_mode='anchor')
+        self.sc.axes.set_xlabel(None)
+        self.sc.axes.set_ylabel("Minutes")
+        self.sc.axes.get_legend().remove()
+        for x, y in enumerate(filtered_df["Minutes"]):
+            self.sc.axes.annotate(f"{y}", (x, y), textcoords='data', fontsize=12)
+        self.sc.fig.tight_layout()
+        self.sc.draw()
+
         total_string = f"Total: {total_mins} mins"
         if total_mins > 60:
             total_string += " / "
@@ -75,29 +141,8 @@ class DataDialog(QDialog):
             avg_string += f"{hrs} hrs " if hrs > 1 else f"{hrs} hr "
             avg_string += f"{mins} mins"
 
-        self.total_label = QLabel(total_string)
-        self.avg_label = QLabel(avg_string)
-
-        self.dialog_layout = QVBoxLayout(self)
-        self.dialog_layout.addWidget(self.choices_dropdown)
-        self.dialog_layout.addWidget(self.custom_date_editor)
-        self.dialog_layout.addWidget(sc)
-        self.dialog_layout.addWidget(self.total_label)
-        self.dialog_layout.addWidget(self.avg_label)
-        
-
-    def filter_df_by_date(self, startdate: datetime, enddate: datetime):
-        filtered_df = self.df[(self.df.index >= startdate) & (self.df.index <= enddate)]
-
-        while startdate <= enddate:
-            if not startdate in filtered_df.index:
-                filtered_df.loc[startdate] = 0 
-            startdate += timedelta(1)
-
-        filtered_df = filtered_df.sort_index()
-        filtered_df.index = filtered_df.index.strftime(f"{date_format} (%a)")
-
-        return filtered_df, filtered_df["Minutes"].sum(), round(filtered_df["Minutes"].mean())
+        self.total_label.setText(total_string)
+        self.avg_label.setText(avg_string)
 
 
 def add_row_to_csv(filename: str, duration_min: int):
